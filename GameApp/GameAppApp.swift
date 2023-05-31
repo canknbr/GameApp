b502b2ef09c0e4975a6421c5d48bd8268f58d9b6
canknbr <cankanbur1@gmail.com>
2023-05-31 20:51:54 +0300

    Initial Commit

---
+++ GameApp/GameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameAppApp.swift
+//  GameApp
+//
+//  Created by Can Kanbur on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
